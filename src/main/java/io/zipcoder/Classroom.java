ae31c345d2cff81bb6c718800cb6aded3df17904
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-06 11:35:52 -0500

    finished gradebook

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import java.util.*;
+
 public class Classroom {
     Student [] classroom;
     public Classroom (){
@@ -21,8 +23,66 @@ public class Classroom {
         }
         return average/classroom.length;
     }
-    //addStudent(Student student)
-    //removeStudent(String firstName, String lastName) move null to end
+    public void addStudent(Student student){
+        for (int i=0;i< classroom.length;i++){
+            if (classroom[i] == null){
+                classroom[i] = student;
+                break;
+            }
+        }
+    }
+    public void removeStudent(String firstName, String lastName) {
+        for (int i=0;i<classroom.length;i++){
+            if (classroom[i].getLastName()==lastName){
+                if (classroom[i].getFirstName() == firstName){
+                    if (i==classroom.length-1)
+                        classroom[i]=null;
+                    else {
+                        classroom[i] = classroom[i + 1];
+                        classroom[i + 1] = null;
+                    }
+                }
+            }
+            if (((i != classroom.length-1) && (classroom[i]==null))
+                    && (classroom[i+1] != null)){
+                classroom[i] = classroom[i + 1];
+                classroom[i + 1] = null;
+            }
+        }
+    }
+    public Student[] getStudentsbyScore(){
+        List <Student> studentSortedList = Arrays.asList(classroom);
+        studentSortedList.sort(Comparator.comparing(Student::getAverageExamScore));
+        return studentSortedList.toArray(new Student[classroom.length]);
+    }
+    public Map<String, String> getGradeBook(){
+        int count=0;
+        Double percentile;
+        String name;
+        Map<String,String> gradeBook = new HashMap<>();
+        for (int i=0; i<classroom.length;i++){
+            if (classroom[i]==null) {
+                count = i + 1;
+                break; //Assuming array has null at end and no students between nulls
+            }
+        }
+        Student[] classroomSorted = this.getStudentsbyScore();
+        for (int i=0;i<count;i++){
+            percentile = ((count-i-1)/(count))*(100.0);
+            name= classroomSorted[i].getFirstName() +classroomSorted[i].getLastName();
+            if (percentile >=90)
+                gradeBook.put(name,"A");
+            else if (percentile <=89 && percentile >=71)
+                gradeBook.put(name,"B");
+            else if (percentile <=70 && percentile >= 50)
+                gradeBook.put(name,"C");
+            else if (percentile <=49 && percentile >=11)
+                gradeBook.put(name,"D");
+            else
+                gradeBook.put(name, "F");
+        }
+        return gradeBook;
+    }
     //getStudentsByScore() sort student by score
 //    getGradeBook()
 //    The class Classroom should define a method getGradeBook() which returns a mapping of Student objects to a respective letter grade determined by creating a grading curve such that
